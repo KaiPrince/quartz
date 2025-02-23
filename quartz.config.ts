1e23659a5d9ce4b5fb90e27359467c2f0206e688
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-02-23 12:32:07 -0800

    feat: personalize
    BREAKING CHANGE

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,8 +16,8 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    baseUrl: process.env.BASE_URL || "localhost",
+    ignorePatterns: [".obsidian", "**/.*", "**/_*", "**/*.png", "**/*.jpg", "**/*.jpeg"],
     defaultDateType: "created",
     generateSocialImages: false,
     theme: {
